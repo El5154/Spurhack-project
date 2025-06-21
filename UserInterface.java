39ba383f9106cdb97d98cf7f4d7468c83962b870
El5154 <dasdas5154@gmail.com>
2025-06-21 16:36:29 -0400

    Changed the options to be more concise
    Changed and added new options

--- UserInterface.java
+++ UserInterface.java
@@ -9,7 +9,7 @@ public class UserInterface{
     }
 
     public void options() {
-        System.out.println("Pick an option: \n1. Add Transaction\n2. Remove Transaction\n3. View Spending");
+        System.out.println("Pick an option: \n  1. Transaction\n  2. Budget\n  3. View Spending\n  4. Savings Goal\n  5. Save and Exit");
         
     }
 }
